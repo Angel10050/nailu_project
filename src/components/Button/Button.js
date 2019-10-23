ab8bec295d9d5eb4404eb3ca9aeceb86d3333c8f
Darwin <50497333+minotad66@users.noreply.github.com>
2019-10-22 19:29:18 -0500

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,16 @@
 import React from "react";
+
 import "./ButtonGeneralStyle.css";
 
+
 const ButtonGeneral = ({ nameBtn, callback }) => {
+
   return (
     <button type="button" class="btn btn-primary button" onClick={callback}>
       {nameBtn}
     </button>
   );
+
 };
+
 export default ButtonGeneral;
