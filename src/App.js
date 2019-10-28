bc7428c8177261c2203373610a4659528ec2768e
angel10050 <angelangel10050@gmail.com>
2019-10-28 13:06:38 -0500

    local commit to make a new branch clean

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Logo from './Components/logo/Logo'
 import './App.css';
-import { animateScroll as scroll } from 'react-scroll'
-import Hero from './Components/heroComponent/Hero'
-import heroImage from './Components/heroComponent/heroMainImage.jpg'
+
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,16 +9,9 @@ class App extends React.Component {
       .then(console.log)
   }
 
-  toScrollup = () => {
-    scroll.scrollToTop()
-}
-
   render() {
     return (
-      <>
-      <Logo onClick={this.toScrollup}/>
-      <Hero heroMainImage={heroImage}/>
-      </>
+      
     );
   }
 }
