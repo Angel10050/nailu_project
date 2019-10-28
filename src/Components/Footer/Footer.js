13dbae740d0640021e928193550847efaaa0dc22
1000VIA <milvampa1@gmail.com>
2019-10-28 13:08:13 -0500

    se corrige lo de quitar el div que sobraba, segun la recomendación de Angel

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,38 +4,38 @@ import '../Footer/Style.css'
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer>
       <div className="container">
-        <h1>Contactos</h1>
-        <div className="contactos">
+        <h2>Contactos</h2>
+        <div>
           <ul className="social-icon">
             <li>
               <a>
-                <span className="fa fa-whatsapp" aria-hidden="true"></span>{' '}
-                <strong>+ 57 314 8169284</strong>{' '}
+                <span className="fa fa-whatsapp" aria-hidden="true"></span>
+                <strong> + 57 314 8169284</strong>
               </a>
             </li>
             <li>
               <a>
-                <span className="fa fa-envelope" aria-hidden="true"></span>{' '}
-                <strong>lida11jaramillo@hotmail.com</strong>{' '}
+                <span className="fa fa-envelope" aria-hidden="true"></span>
+                <strong> lida11jaramillo@hotmail.com</strong>
               </a>
             </li>
             <li>
               <a href="https://www.facebook.com/Nailu-130735667644830/">
-                <i className="fa fa-facebook-official" aria-hidden="true"></i>{' '}
-                <strong>@nailu</strong>{' '}
+                <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                <strong> @nailu</strong>
               </a>
             </li>
             <li>
               <a href="https://www.instagram.com/nailu511/">
-                <i className="fa fa-instagram" aria-hidden="true"></i>{' '}
-                <strong>@nailu511</strong>{' '}
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+                <strong> @nailu511</strong>
               </a>
             </li>
-            <p>&copy;Nailu_Project Todos los derechos reservados 2019.</p>
           </ul>
         </div>
+        <p>&copy; DTW - Todos los derechos reservados 2019.</p>
       </div>
     </footer>
   )
