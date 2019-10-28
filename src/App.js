1e5dfa7b76af21534766403bfffc355ff14583c5
Darwin <50497333+minotad66@users.noreply.github.com>
2019-10-28 12:32:43 -0500

    NavComponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import './App.css';
 
 import { animateScroll as scroll } from 'react-scroll'
 
+import Hero from './Components/heroComponent/Hero'
+import heroImage from './Components/heroComponent/hands-1751637_1280.jpg'
+import ButtonGeneral from './Components/Button/Button'
 import NavComponent from './Components/navComponent/NavComponent'
 
 class App extends React.Component {
   componentDidMount() {
-    fetch('/api/data')
+    fetch("/api/data")
       .then(response => response.json())
       .then(console.log);
   }
@@ -24,7 +27,15 @@ class App extends React.Component {
     return (
       <>
       <NavComponent toScrollup={this.toScrollup} />
+      <Hero heroMainImage={heroImage}/>
+      <div>
+        <ButtonGeneral nameBtn="Inscribete" callback={this.handlerClick} />
+        <ButtonGeneral nameBtn="Mas informacion" callback={this.handlerClick} />
+        <ButtonGeneral nameBtn="Agendar" callback={this.handlerClick} />
+        <ButtonGeneral nameBtn="Agendar tebgrvfacdsfg" callback={this.handlerClick}/>
+      </div>
       </>
+
     );
   }
 }
