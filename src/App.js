3d72f12f6dc97e1aa1a783058488d89ceeffafc5
angel10050 <angelangel10050@gmail.com>
2019-10-31 18:31:56 -0500

    responsive HeroImage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
+
 import { animateScroll as scroll } from 'react-scroll'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Hero from './Components/heroComponent/Hero'
 import heroImage from './Components/heroComponent/hands-1751637_1280.jpg'
 import ButtonGeneral from './Components/Button/Button'
 import NavComponent from './Components/navComponent/NavComponent'
 import Footer from './Components/Footer/Footer'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Form from './Components/form/Form'
 import Login from './Components/adminPage/Login/loginUser'
 
 class App extends React.Component {
@@ -28,6 +31,10 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/admin">
               <Login />
+            </Route>
+
+            <Route exact path="/user/info">
+              <Form />
             </Route>
 
             <Route>
