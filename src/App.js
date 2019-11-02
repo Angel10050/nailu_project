c8dfa6b320d90d8070f76e26f6025843bae2dd28
angel10050 <angelangel10050@gmail.com>
2019-11-02 17:19:46 -0500

    fixing some problems of styles and methods

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React from 'react'
 import './App.css'
 
 import { animateScroll as scroll } from 'react-scroll'
-
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 
 import Hero from './Components/heroComponent/Hero'
 import heroImage from './Components/heroComponent/hands-1751637_1280.jpg'
-
 import ButtonGeneral from './Components/Button/Button'
 import NavComponent from './Components/navComponent/NavComponent'
 import Footer from './Components/Footer/Footer'
-
-
-import AdminPage from './Components/AdminPage/AdminPage'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Login from './Components/AdminPage/Login/loginUser'
 import Form from './Components/form/Form'
-
+import Login from './Components/AdminPage/Login/loginUser'
+import AdminPage from './Components/AdminPage/AdminPage'
 
 class App extends React.Component {
   componentDidMount() {
@@ -35,41 +27,51 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router> 
-         <Switch>
-
-         <Route exact path="/admin">
+      <>
+        <Router>
+          <Switch>
+            <Route exact path="/admin">
               <Login />
-              
-         </Route>
-
-          <Route exact path='/admin/content'>
-
-            <AdminPage />
-
-          </Route>
-          <Route>
+            </Route>
 
             <Route exact path="/user/form">
               <Form />
             </Route>
 
-            <Route>
+            <Route exact path="/admin/content">
+              <AdminPage />
+            </Route>
 
+            <Route>
               <NavComponent toScrollup={this.toScrollup} />
-                <Hero heroMainImage={heroImage}/>
+              <Hero heroMainImage={heroImage} />
               <div>
-                <ButtonGeneral type='button' nameBtn='Inscribete' callback={this.handlerClick} />
-                <ButtonGeneral type='button' nameBtn='Mas informacion' callback={this.handlerClick} />
-                <ButtonGeneral type='button' nameBtn='Agendar' callback={this.handlerClick} />
-                <ButtonGeneral type='button' nameBtn='Agendar cualquier cosa' callback={this.handlerClick}/>
+                <ButtonGeneral
+                  type="button"
+                  nameBtn="Inscribete"
+                  callback={this.handlerClick}
+                />
+                <ButtonGeneral
+                  type="button"
+                  nameBtn="Mas informacion"
+                  callback={this.handlerClick}
+                />
+                <ButtonGeneral
+                  type="button"
+                  nameBtn="Agendar"
+                  callback={this.handlerClick}
+                />
+                <ButtonGeneral
+                  type="button"
+                  nameBtn="Agendar cualquier cosa"
+                  callback={this.handlerClick}
+                />
               </div>
               <Footer />
-
-          </Route>
-
-         </Switch>
-      </Router>
+            </Route>
+          </Switch>
+        </Router>
+      </>
     )
   }
 }
