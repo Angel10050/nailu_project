dd2b4100c6152b5fd18c6d1378b8e5839b030d58
angel10050 <angelangel10050@gmail.com>
2019-11-03 22:05:39 -0500

    delete no used component from import section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Hero from './Components/heroComponent/Hero'
 import heroImage from './Components/heroComponent/hands-1751637_1280.jpg'
-// import ButtonGeneral from './Components/Button/Button'
 import NavComponent from './Components/navComponent/NavComponent'
 import BoxComponent from './Components/BoxComponent/BoxComponent'
 import Footer from './Components/Footer/Footer'
