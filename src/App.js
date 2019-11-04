450b5afe6f892a4debe63f4b1d8ea6d63810364a
angel10050 <angelangel10050@gmail.com>
2019-11-03 20:11:27 -0500

    no boostrap in boxComponent

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Hero from './Components/heroComponent/Hero'
 import heroImage from './Components/heroComponent/hands-1751637_1280.jpg'
-import ButtonGeneral from './Components/Button/Button'
+
 import NavComponent from './Components/navComponent/NavComponent'
 import BoxComponent from './Components/BoxComponent/BoxComponent'
 import Footer from './Components/Footer/Footer'
 import Form from './Components/form/Form'
 import Login from './Components/adminPage/Login/loginUser'
-import { ListGroupItem } from 'react-bootstrap'
 
 class App extends React.Component {
   componentDidMount() {
@@ -33,7 +32,7 @@ class App extends React.Component {
         dia: '31',
         mes: 'Octubre',
         descripcion:
-          'Maquillaje ArtisticoO Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quose.'
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quose.'
       },
       {
         imagenUrl: 'https://i.ibb.co/V9M7KCL/3.jpg',
