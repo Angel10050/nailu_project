a14e883ebf3eb31287c6841e95d4b6d119b05d01
afmeva <afmeva@gmail.com>
2019-11-03 23:26:47 -0500

    Fix dist route

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParse = require('body-parser')
 const path = require('path')
 
-const distDir = path.join(__dirname, "../", "build");
+const distDir = path.join(__dirname, "../../", "build");
 
 const authInit = require('./routes/admin')
 const contactInit = require('./routes/contact')
