973a6310c2fa619f22f97ea327827d62a0660970
angel10050 <angelangel10050@gmail.com>
2019-11-06 15:40:25 -0500

    a different image

--- src/Components/AdminPage/Login/loginUser.js
+++ src/Components/AdminPage/Login/loginUser.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react'
 
 import '../Login/styleLogin.css'
+import Logo from '../../logo/Logo'
 import { Form } from 'react-bootstrap'
 
 class Login extends Component {
   state = {
-    user: []
-  }
+      username: '',
+      password: ''
 
-  getInfo = () => {
-    fetch('/api/login')
-      .then(response => response.JSON())
-      .then(messages => {
-        console.log('messages')
-      })
-      .catch(error => {
-        alert('Ha ocurrido un error', error)
-      })
-  }
-
-  componentDidMount() {
-    this.getInfo()
   }
 
   handleSubmit = event => {
     event.preventDefault()
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.login)
+    })
+      .then(response => response.json())
+      .then(console.log)
+      .catch(Error)
   }
 
   handleOnChange = event => {
@@ -36,73 +34,78 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="container login containerLogin">
-        <div className="row loginRow">
-          <div className="login-wrap">
-            <Form
-              onSubmit={this.handleSubmit}
-              className="form-horizontal"
-              role="form"
-            >
-              <div className="form-group">
-                <label className="col-sm-3 control-label textLogin">
-                  Usuario
-                </label>
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="basic-addon1">
-                      <i className="fa fa-user loginI"></i>
-                    </span>
+        <>
+        <div className="container login containerLogin">
+          <header className="adminHeader">
+            <Logo />
+          </header>
+          <div className="row loginRow">
+            <div className="login-wrap">
+              <Form
+                onSubmit={this.handleSubmit}
+                className="form-horizontal"
+                role="form"
+              >
+                <div className="form-group">
+                  <label className="col-sm-3 control-label textLogin">
+                    Usuario
+                  </label>
+                  <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text" id="basic-addon1">
+                        <i className="fa fa-user loginI"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      value={this.state.username}
+                      name="username"
+                      onChange={this.handleOnChange}
+                      placeholder="NombreUsuario"
+                      className="form-control inputLogin"
+                      required
+                    ></input>
                   </div>
-                  <input
-                    type="text"
-                    value={this.state.user}
-                    name="user"
-                    onChange={this.handleOnChange}
-                    placeholder="NombreUsuario"
-                    className="form-control inputLogin"
-                    required
-                  ></input>
                 </div>
-              </div>
 
-              <div className="form-group">
-                <label className="col-sm-3 control-label textLogin">
-                  Contaseña
-                </label>
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="basic-addon1">
-                      <i className="fa fa-unlock-alt"></i>
-                    </span>
+                <div className="form-group">
+                  <label className="col-sm-3 control-label textLogin">
+                    Contaseña
+                  </label>
+                  <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text" id="basic-addon1">
+                        <i className="fa fa-unlock-alt"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="password"
+                      className="form-control inputLogin"
+                      value={this.state.password}
+                      name="password"
+                      onChange={this.handleOnChange}
+                      placeholder="Contraseña"
+                      required
+                    ></input>
                   </div>
-                  <input
-                    type="password"
-                    className="form-control inputLogin"
-                    value={this.state.password}
-                    name="password"
-                    onChange={this.handleOnChange}
-                    placeholder="Contraseña"
-                    required
-                  ></input>
                 </div>
-              </div>
-              <div className="form-group">
-                <div className="col-sm-offset-3 col-sm-9">
-                  <div className="checkbox"></div>
+                <div className="form-group">
+                  <div className="col-sm-offset-3 col-sm-9">
+                    <div className="checkbox"></div>
+                  </div>
                 </div>
-              </div>
-              <div className="form-group last d-flex justify-content-center">
-                <div className="col-sm-offset-3 row-sm-5 ">
-                  <button type="submit" className="btn btn-primary btn-sm">
-                    Ingresar
-                  </button>
+                <div className="form-group last d-flex justify-content-center">
+                  <div className="col-sm-offset-3 row-sm-5 ">
+                    <button type="submit" className="btn btn-primary btn-sm">
+                      Ingresar
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </Form>
+              </Form>
+            </div>
           </div>
-        </div>
       </div>
+    </>
     )
   }
 }
