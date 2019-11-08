8344fb5e9a2036d14f3f810d118595eb83c331e0
angel10050 <angelangel10050@gmail.com>
2019-11-08 17:11:07 -0500

    same info in server that in react state

--- src/Components/AdminPage/AdminForm/AdminForm.js
+++ src/Components/AdminPage/AdminForm/AdminForm.js
@@ -9,13 +9,14 @@ class AdminForm extends Component{
             day : '',
             month : '',
             description : '', 
-            eventImage : ''
+            image : undefined
         }
 }
 
     handleSubmit = (event) => {
+        console.log(this.state)
         event.preventDefault()
-        fetch('api/trainings',{
+        fetch('/api/training',{
             method : 'POST',
             headers : {
                 'Content-Type': 'application/json'
@@ -23,8 +24,11 @@ class AdminForm extends Component{
             body : JSON.stringify(this.state.eventData)
         })
         .then(response => response.json())
-        .then(() => this.setState({error : false}))
-        .catch(() => this.setState({error : true}))
+        .then(console.log)
+        .catch(console.log)
+        // .then(response => response.json())
+        // .then(() => this.setState({error : false}))
+        // .catch(() => this.setState({error : true}))
      }
 
     handleChange = (event) => {
@@ -46,12 +50,12 @@ class AdminForm extends Component{
                         <div className='formEventDate'>
                             <div>
                                 <label htmlFor='day' className='adminLabel'>Dia del evento:</label>
-                                <input value={this.state.day} type='number' name='day' id='day' className='adminInput inputDay' onChange={this.handleChange} />
+                                <input value={this.state.eventData.day} type='number' name='day' id='day' className='adminInput inputDay' onChange={this.handleChange} />
                             </div>
 
                             <div>
                                 <label htmlFor='month' className='adminLabel'>Mes del evento:</label>
-                                <input value={this.state.month} type='text' list='months' name='month' id='month' className='adminInput' onChange={this.handleChange} />
+                                <input value={this.state.eventData.month} type='text' list='months' name='month' id='month' className='adminInput' onChange={this.handleChange} />
                                     <datalist id='months'>
                                         <option value='Enero' />
                                         <option value='Frebrero' />
@@ -72,10 +76,10 @@ class AdminForm extends Component{
                         <div className='formEventContent'>
                             
                                 <label htmlFor='description' className='adminLabel'>Describre el evento (Se breve):</label>
-                                <textarea value={this.state.description} name='description' id='description' className='adminInput textarea' onChange={this.handleChange}/>
+                                <textarea value={this.state.eventData.description} name='description' id='description' className='adminInput textarea' onChange={this.handleChange}/>
 
                                 <label htmlFor='eventImage' className='adminLabel labelButton'>Elige una imagen:</label> 
-                                <input value={this.state.eventImage} type='file' name='eventImage' id='eventImage' className='adminInput' onChange={this.handleChange} />
+                                <input value={this.state.eventData.eventImage} type='file' name='image' id='image' className='adminInput' onChange={this.handleChange} />
 
                         </div>
 
