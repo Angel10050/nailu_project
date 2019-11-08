673b8e3d44c3cf89e19fe1b65185b8994b204656
angel10050 <angelangel10050@gmail.com>
2019-11-08 02:38:29 -0500

    formAdminContent handler error end user feedBack, also method POST fetch

--- src/Components/AdminPage/AdminForm/AdminForm.js
+++ src/Components/AdminPage/AdminForm/AdminForm.js
@@ -4,66 +4,92 @@ import Button from '../../Button/Button'
 
 class AdminForm extends Component{
     state={
+        error : false,
+        eventData : {
             day : '',
             month : '',
             description : '', 
             eventImage : ''
         }
+}
 
     handleSubmit = (event) => {
         event.preventDefault()
+        fetch('api/trainings',{
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(this.state.eventData)
+        })
+        .then(response => response.json())
+        .then(console.log)
+        .catch(this.setState({error : true}))
      }
 
     handleChange = (event) => {
-        const {id, value} = event.target
-         this.setState({
-                [id] : value
-         })      
+        const {name, value} = event.target
+         this.setState(prevState => ({
+             eventData : {
+                ...prevState.eventData,
+                    [name]:value
+                }
+            })
+         )      
     }
     render(){
         return(
             <>
-                <form className='adminForm' onSubmit={this.handleSubmit} method='post'>
+                <div className='formContainerMain'>
+                    <form className='adminForm' onSubmit={this.handleSubmit} method='post'>
 
-                    <div className='formEventDate'>
-                        <div>
-                            <label htmlFor='day' className='adminLabel'>Dia del evento:</label>
-                            <input value={this.state.day} type='number' name='day' id='day' className='adminInput inputDay' onChange={this.handleChange} />
+                        <div className='formEventDate'>
+                            <div>
+                                <label htmlFor='day' className='adminLabel'>Dia del evento:</label>
+                                <input value={this.state.day} type='number' name='day' id='day' className='adminInput inputDay' onChange={this.handleChange} />
+                            </div>
+
+                            <div>
+                                <label htmlFor='month' className='adminLabel'>Mes del evento:</label>
+                                <input value={this.state.month} type='text' list='months' name='month' id='month' className='adminInput' onChange={this.handleChange} />
+                                    <datalist id='months'>
+                                        <option value='Enero' />
+                                        <option value='Frebrero' />
+                                        <option value='Marzo' />
+                                        <option value='Abril' />
+                                        <option value='Mayo' />
+                                        <option value='Junio' />
+                                        <option value='Julio' />
+                                        <option value='Agosto' />
+                                        <option value='Septiembre' />
+                                        <option value='Octubre' />
+                                        <option value='Noviembre' />
+                                        <option value='Diciembre' />
+                                    </datalist>
+                            </div>
                         </div>
 
-                        <div>
-                            <label htmlFor='month' className='adminLabel'>Mes del evento:</label>
-                            <input value={this.state.month} type='text' list='months' name='month' id='month' className='adminInput' onChange={this.handleChange} />
-                                <datalist id='months'>
-                                    <option value='Enero' />
-                                    <option value='Frebrero' />
-                                    <option value='Marzo' />
-                                    <option value='Abril' />
-                                    <option value='Mayo' />
-                                    <option value='Junio' />
-                                    <option value='Julio' />
-                                    <option value='Agosto' />
-                                    <option value='Septiembre' />
-                                    <option value='Octubre' />
-                                    <option value='Noviembre' />
-                                    <option value='Diciembre' />
-                                </datalist>
+                        <div className='formEventContent'>
+                            
+                                <label htmlFor='description' className='adminLabel'>Describre el evento (Se breve):</label>
+                                <textarea value={this.state.description} name='description' id='description' className='adminInput textarea' onChange={this.handleChange}/>
+
+                                <label htmlFor='eventImage' className='adminLabel labelButton'>Elige una imagen:</label> 
+                                <input value={this.state.eventImage} type='file' name='eventImage' id='eventImage' className='adminInput' onChange={this.handleChange} />
+
                         </div>
-                    </div>
 
-                    <div className='formEventContent'>
-                        
-                            <label htmlFor='description' className='adminLabel'>Describre el evento (Se breve):</label>
-                            <textarea value={this.state.description} name='description' id='description' className='adminInput textarea' onChange={this.handleChange}/>
+                        <Button type={'submit'} nameBtn={'Confirmar'} className='adminButtonForm'/>    
 
-                            <label htmlFor='eventImage' className='adminLabel labelButton'>Elige una imagen:</label> 
-                            <input value={this.state.eventImage} type='file' name='eventImage' id='eventImage' className='adminInput' onChange={this.handleChange} />
-        
-                    </div>
+                    </form> 
 
-                <Button type={'submit'} nameBtn={'Confirmar'} className='adminButtonForm'/>    
-                
-                </form> 
+                {this.state.error ? 
+                    <p className="errorMensage">
+                            Datos invalidos intenta nuevamente
+                    </p> 
+                    : ''
+                }
+                </div>   
             </>
         )
     }
