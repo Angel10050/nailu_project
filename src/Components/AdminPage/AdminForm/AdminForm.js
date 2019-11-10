37a331dc556fe089754995dc2363e0bef8adf445
1000VIA <milvampa1@gmail.com>
2019-11-09 23:20:02 -0500

    Add Token with validations

--- src/Components/AdminPage/AdminForm/AdminForm.js
+++ src/Components/AdminPage/AdminForm/AdminForm.js
@@ -1,95 +1,139 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import '../AdminForm/adminFormStyle.css'
 import Button from '../../Button/Button'
 
-class AdminForm extends Component{
-    state={
-        error : null,
-        eventData : {
-            day : '',
-            month : '',
-            description : '', 
-            eventImage : ''
-        }
-}
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        fetch('api/trainings',{
-            method : 'POST',
-            headers : {
-                'Content-Type': 'application/json'
-            },
-            body : JSON.stringify(this.state.eventData)
-        })
-        .then(response => response.json())
-        .then(() => this.setState({error : false}))
-        .catch(() => this.setState({error : true}))
-     }
-
-    handleChange = (event) => {
-        const {name, value} = event.target
-         this.setState(prevState => ({
-             eventData : {
-                ...prevState.eventData,
-                    [name]:value
-                }
-            })
-         )      
+class AdminForm extends Component {
+  state = {
+    error: null,
+    eventData: {
+      day: '',
+      month: '',
+      description: '',
+      eventImage: ''
     }
-    render(){
-        return(
-            <>
-                <div className='formContainerMain'>
-                    <form className='adminForm' onSubmit={this.handleSubmit} method='post'>
+  }
 
-                        <div className='formEventDate'>
-                            <div>
-                                <label htmlFor='day' className='adminLabel'>Dia del evento:</label>
-                                <input value={this.state.day} type='number' name='day' id='day' className='adminInput inputDay' onChange={this.handleChange} />
-                            </div>
+  handleSubmit = event => {
+    event.preventDefault()
+    fetch('api/trainings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.eventData)
+    })
+      .then(response => response.json())
+      .then(() => this.setState({ error: false }))
+      .catch(() => this.setState({ error: true }))
+  }
 
-                            <div>
-                                <label htmlFor='month' className='adminLabel'>Mes del evento:</label>
-                                <input value={this.state.month} type='text' list='months' name='month' id='month' className='adminInput' onChange={this.handleChange} />
-                                    <datalist id='months'>
-                                        <option value='Enero' />
-                                        <option value='Frebrero' />
-                                        <option value='Marzo' />
-                                        <option value='Abril' />
-                                        <option value='Mayo' />
-                                        <option value='Junio' />
-                                        <option value='Julio' />
-                                        <option value='Agosto' />
-                                        <option value='Septiembre' />
-                                        <option value='Octubre' />
-                                        <option value='Noviembre' />
-                                        <option value='Diciembre' />
-                                    </datalist>
-                            </div>
-                        </div>
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState(prevState => ({
+      eventData: {
+        ...prevState.eventData,
+        [name]: value
+      }
+    }))
+  }
+  render() {
+    return (
+      <>
+        <div className="formContainerMain">
+          <form
+            className="adminForm"
+            onSubmit={this.handleSubmit}
+            method="post"
+          >
+            <div className="formEventDate">
+              <div>
+                <label htmlFor="day" className="adminLabel">
+                  Dia del evento:
+                </label>
+                <input
+                  value={this.state.day}
+                  type="number"
+                  name="day"
+                  id="day"
+                  className="adminInput inputDay"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-                        <div className='formEventContent'>
-                            
-                                <label htmlFor='description' className='adminLabel'>Describre el evento (Se breve):</label>
-                                <textarea value={this.state.description} name='description' id='description' className='adminInput textarea' onChange={this.handleChange}/>
+              <div>
+                <label htmlFor="month" className="adminLabel">
+                  Mes del evento:
+                </label>
+                <input
+                  value={this.state.month}
+                  type="text"
+                  list="months"
+                  name="month"
+                  id="month"
+                  className="adminInput"
+                  onChange={this.handleChange}
+                />
+                <datalist id="months">
+                  <option value="Enero" />
+                  <option value="Frebrero" />
+                  <option value="Marzo" />
+                  <option value="Abril" />
+                  <option value="Mayo" />
+                  <option value="Junio" />
+                  <option value="Julio" />
+                  <option value="Agosto" />
+                  <option value="Septiembre" />
+                  <option value="Octubre" />
+                  <option value="Noviembre" />
+                  <option value="Diciembre" />
+                </datalist>
+              </div>
+            </div>
 
-                                <label htmlFor='eventImage' className='adminLabel labelButton'>Elige una imagen:</label> 
-                                <input value={this.state.eventImage} type='file' name='eventImage' id='eventImage' className='adminInput' onChange={this.handleChange} />
+            <div className="formEventContent">
+              <label htmlFor="description" className="adminLabel">
+                Describre el evento (Se breve):
+              </label>
+              <textarea
+                value={this.state.description}
+                name="description"
+                id="description"
+                className="adminInput textarea"
+                onChange={this.handleChange}
+              />
 
-                        </div>
+              <label htmlFor="eventImage" className="adminLabel labelButton">
+                Elige una imagen:
+              </label>
+              <input
+                value={this.state.eventImage}
+                type="file"
+                name="eventImage"
+                id="eventImage"
+                className="adminInput"
+                onChange={this.handleChange}
+              />
+            </div>
 
-                        <Button type={'submit'} nameBtn={'Confirmar'} className='adminButtonForm'/>    
-
-                    </form> 
-                  {
-                    this.state.error ? <p className='errorMensage'>Error en los datos intentalo nuevamente</p> :
-                    this.state.error === false ? <p className='errorMensage'>Envio exitoso</p> : '' 
-                  }  
-                </div>   
-            </>
-        )
-    }
+            <Button
+              type={'submit'}
+              nameBtn={'Confirmar'}
+              className="adminButtonForm"
+            />
+          </form>
+          {this.state.error ? (
+            <p className="errorMensage">
+              Error en los datos intentalo nuevamente
+            </p>
+          ) : this.state.error === false ? (
+            <p className="errorMensage">Envio exitoso</p>
+          ) : (
+            ''
+          )}
+        </div>
+      </>
+    )
+  }
 }
 
 export default AdminForm
